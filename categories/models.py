6b63bdb43ec4bdc10232f67cf6f9b681db63300d
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2021-09-16 21:57:42 +0600

    Category model created

---
+++ categories/models.py
@@ -0,0 +1,15 @@
+from time import strftime
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from utils.common_utils import generate_file_name
+
+
+def image_directory_path(instance, filename):
+    return 'categories/{0}/{1}'.format(strftime('%Y/%m/%d'), generate_file_name() + '.' + filename.split('.')[-1])
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, verbose_name=_("Category name"))
+    image = models.ImageField(upload_to=image_directory_path, verbose_name=_("Image"))
