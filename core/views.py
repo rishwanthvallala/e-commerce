83dbe4374e2f89dea37bf39f448e0c5c20d64a80
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2021-09-17 22:26:58 +0600

    show all products in home

--- core/views.py
+++ core/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
 from categories.models import Category
+from products.models import Product
 
 
 def index(request):
     categories = Category.objects.all()
+    products = Product.objects.all()
     context = {
-        'categories': categories
+        'categories': categories,
+        'products': products,
     }
     return render(request, 'index.html', context)
