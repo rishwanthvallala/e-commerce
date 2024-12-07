7cf4ac1f73a9a4bd52e208908b65f888e80a876b
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-12-07 23:05:18 +0600

    ✨ feat(logout): logout from header menu

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import UserLoginView, UserSignupView, forgot_password
-from django.contrib.auth.views import LogoutView
+from .views import UserLoginView, UserSignupView, forgot_password, logout_view
+from django.views.decorators.http import require_http_methods
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('signup/', UserSignupView.as_view(), name='signup'),
-    path('logout/', LogoutView.as_view(next_page='users:login'), name='logout'),
+    path('logout/', require_http_methods(['POST'])(logout_view), name='logout'),
     path('forgot-password/', forgot_password, name='forgot-password'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
 from django.views.generic import FormView, CreateView
 from django.urls import reverse_lazy
 from .forms import UserLoginForm, UserRegistrationForm
@@ -12,6 +12,11 @@ class UserLoginView(FormView):
     template_name = "users/auth/login.html"
     form_class = UserLoginForm
     success_url = reverse_lazy("core:home")
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('core:home')
+        return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
         user = form.get_user()
@@ -29,6 +34,11 @@ class UserSignupView(CreateView):
     form_class = UserRegistrationForm
     success_url = reverse_lazy("users:login")
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('core:home')
+        return super().dispatch(request, *args, **kwargs)
+
     def form_invalid(self, form):
         # This will return the form with errors to the template
         return self.render_to_response(self.get_context_data(form=form))
@@ -41,3 +51,10 @@ class UserSignupView(CreateView):
 
 def forgot_password(request):
     return render(request, "users/auth/forgot-password.html")
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('users:login')
+    return redirect('core:home')
