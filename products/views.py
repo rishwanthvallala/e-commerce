aed6bf86b8d4a0d04e846cee2827310efff38176
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-12-09 00:23:17 +0600

    feat(product): product details page

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 
 from .models import Product
 from categories.models import Category
@@ -22,6 +23,29 @@ class ProductDetailView(DetailView):
         if obj is None:
             raise Http404("Product not found")
         return obj
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Get similar products from the same category
+        similar_products = Product.objects.filter(
+            category=self.object.category,
+            is_active=True
+        ).exclude(
+            id=self.object.id
+        )[:6]
+        
+        # Get related products (from all categories)
+        related_products = Product.objects.filter(
+            is_active=True
+        ).exclude(
+            id=self.object.id
+        ).exclude(
+            id__in=[p.id for p in similar_products]
+        )[:8]  # Limit to 8 products
+        
+        context['similar_products'] = similar_products
+        context['related_products'] = related_products
+        return context
 
 
 class ProductsByCategoryView(ListView):
