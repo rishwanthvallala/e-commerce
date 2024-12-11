e4db1320ad4159a110b27277d62386ab3fcb98a2
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-12-11 20:32:54 +0600

    feat(category): updated category model to include category status

--- categories/models.py
+++ categories/models.py
@@ -7,14 +7,26 @@ from utils.common_utils import generate_file_name
 
 
 def image_directory_path(instance, filename):
-    return 'categories/{0}/{1}'.format(strftime('%Y/%m/%d'), generate_file_name() + '.' + filename.split('.')[-1])
+    return "categories/{0}/{1}".format(
+        strftime("%Y/%m/%d"), generate_file_name() + "." + filename.split(".")[-1]
+    )
 
 
 class Category(models.Model):
+    class StatusChoices(models.TextChoices):
+        ACTIVE = "active", "Active"
+        INACTIVE = "inactive", "Inactive"
+
     name = models.CharField(max_length=250, verbose_name=_("Name"))
     slug = models.SlugField(unique=True, verbose_name=_("Slug"))
     image = models.ImageField(upload_to=image_directory_path, verbose_name=_("Image"))
-    
+    status = models.CharField(
+        max_length=10,
+        choices=StatusChoices.choices,
+        default=StatusChoices.ACTIVE,
+        verbose_name=_("Status"),
+    )
+
     class Meta:
         verbose_name = _("Category")
         verbose_name_plural = _("Categories")
