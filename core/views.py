ff396c9a1e4ed8a08996f29d523e7e1994d7a90f
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-12-14 22:32:34 +0600

    feat: add wishlist status annotation to products and update UI for wishlisted items

--- core/views.py
+++ core/views.py
@@ -1,36 +1,53 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.db.models import Exists, OuterRef
+from django.db import models
 
-from categories.models import Category
 from products.models import Product
+from wishlist.models import Wishlist
+
 
 def index(request):
     products = Product.objects.all()
+
+    if request.user.is_authenticated:
+        # Annotate products with wishlist status for the current user
+        products = products.annotate(
+            is_wishlisted=Exists(
+                Wishlist.objects.filter(user=request.user, product=OuterRef("pk"))
+            )
+        )
+    else:
+        # For unauthenticated users, set is_wishlisted to False
+        products = products.annotate(
+            is_wishlisted=models.Value(False, output_field=models.BooleanField())
+        )
+
     context = {
-        'products': products,
+        "products": products,
     }
-    return render(request, 'index.html', context)
+    return render(request, "index.html", context)
 
 
 class AboutView(TemplateView):
-    template_name = 'core/about.html'
+    template_name = "core/about.html"
 
 
 class ContactView(TemplateView):
-    template_name = 'core/contact.html'
+    template_name = "core/contact.html"
 
 
 class TermsView(TemplateView):
-    template_name = 'core/terms.html'
+    template_name = "core/terms.html"
 
 
 class PrivacyView(TemplateView):
-    template_name = 'core/privacy.html'
+    template_name = "core/privacy.html"
 
 
 class FAQView(TemplateView):
-    template_name = 'core/faq.html'
+    template_name = "core/faq.html"
 
 
 class RefundView(TemplateView):
-    template_name = 'core/refund.html'
+    template_name = "core/refund.html"
